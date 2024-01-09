6facfb0ec2176eed440caabb5e4356a8007a9304
l.r0y <122097263+lroy-stack@users.noreply.github.com>
2024-01-09 13:12:14 +0100

    Add files via upload

--- itinerary.js
+++ itinerary.js
@@ -11,3 +11,39 @@ document.querySelectorAll('.tooltip').forEach(item => {
         item.querySelector('.tooltiptext').style.opacity = 0;
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtén todos los detalles del día y los puntos en el mapa
+    const days = document.querySelectorAll('.day');
+    const tooltips = document.querySelectorAll('.tooltip');
+
+    // Función para mostrar el tooltip correspondiente
+    function showTooltip(id) {
+        tooltips.forEach(tooltip => {
+            const tooltipDay = tooltip.getAttribute('data-day');
+            if(tooltipDay === id) {
+                tooltip.querySelector('.tooltiptext').style.visibility = 'visible';
+            }
+        });
+    }
+
+    // Función para ocultar todos los tooltips
+    function hideTooltips() {
+        tooltips.forEach(tooltip => {
+            tooltip.querySelector('.tooltiptext').style.visibility = 'hidden';
+        });
+    }
+
+    // Agrega event listeners a cada detalle del día
+    days.forEach(day => {
+        day.addEventListener('mouseenter', function() {
+            // Muestra el tooltip correspondiente al día
+            showTooltip(this.id);
+        });
+
+        day.addEventListener('mouseleave', function() {
+            // Oculta todos los tooltips
+            hideTooltips();
+        });
+    });
+});
